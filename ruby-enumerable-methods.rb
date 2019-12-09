9b005644e834f7ab8782b578e4fefab10b851da2
firmo holanda <firmo@viize.com>
2019-12-09 19:27:08 -0300

    my_each_with_index implemented

--- ruby-enumerable-methods.rb
+++ ruby-enumerable-methods.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
 module Enumerable
+  
   def my_each
     for item in self do
       yield(item)
+    end
+  end
+
+  def my_each_with_index
+    i = 0
+    for item in self do
+      yield(item, i)
+      i += 1
     end
   end
 
@@ -11,4 +20,5 @@ end
 
 my_array = ["firmo", "pinheiro", "holanda"]
 
-my_array.my_each { |i| puts i}
+#my_array.my_each { |i| puts i}
+my_array.my_each_with_index { |item, i| puts item + " " + i.to_s}
