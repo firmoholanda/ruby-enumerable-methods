a63ded9366302f957e2a45bf81ff452b7779f3f0
firmo holanda <firmo@viize.com>
2019-12-09 19:35:23 -0300

    my_select

--- ruby-enumerable-methods.rb
+++ ruby-enumerable-methods.rb
@@ -16,9 +16,21 @@ module Enumerable
     end
   end
 
+  def my_select
+    select_arr = []
+    self.my_each do |item|
+      select_arr << item if yield(item)
+    end
+    select_arr
+  end
+
 end
 
-my_array = ["firmo", "pinheiro", "holanda"]
+#my_array = ["firmo", "pinheiro", "holanda"]
+my_array = [3, 5, 1, 10, 7, 12]
 
 #my_array.my_each { |i| puts i}
-my_array.my_each_with_index { |item, i| puts item + " " + i.to_s}
+#my_array.my_each_with_index { |item, i| puts item + " " + i.to_s}
+
+puts my_array.my_select { |num|  num.even?  }
+
