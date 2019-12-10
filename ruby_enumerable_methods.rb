415d3e768061710f341da21e86096f3f462f3604
firmo holanda <firmo@viize.com>
2019-12-10 17:40:23 -0300

    Update ruby_enumerable_methods.rb

--- ruby_enumerable_methods.rb
+++ ruby_enumerable_methods.rb
@@ -59,7 +59,7 @@ module Enumerable
     return self unless my_proc || block_given?
 
     for item in self do
-     return block_given? ? new_arr << yield(item) : new_arr << code.call(item)
+      return block_given? ? new_arr << yield(item) : new_arr << code.call(item)
     end
     new_arr
   end
