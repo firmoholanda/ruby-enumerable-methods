ea83574b0b1368beec9e10e28181b31d6b2dd991
firmo holanda <firmo@viize.com>
2019-12-17 14:20:11 -0300

    added #my_each

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,7 +1,17 @@
-
-require_relative '../enumerable_methods'
+require './ruby_enumerable_methods.rb'
 
 RSpec.describe Enumerable do
-  let(:arr) { [1, 2, 3, 4] }
+  let(:my_array) { [3, 5, 1, 10, 7, 12, 33] }
+
+  # my_each
+  describe '#my_each' do
+    it 'returns enum if block not given' do
+      expect(my_array.my_each).to be_an Enumerator
+    end
+
+    it 'returns self if block given' do
+      expect(my_array.my_each { |x| x + 1 }).to eql(my_array)
+    end
+  end
 
 end
