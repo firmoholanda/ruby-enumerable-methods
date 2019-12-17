1d9621a5bef5f4bb0c0bc4b047f7fd2a628a2f5b
firmo holanda <firmo@viize.com>
2019-12-17 14:34:37 -0300

    added my_each_with_index

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -5,12 +5,23 @@ RSpec.describe Enumerable do
 
   # my_each
   describe '#my_each' do
-    it 'returns enum if block not given' do
+    it "returns enumerator if block not is given" do
       expect(my_array.my_each).to be_an Enumerator
     end
 
-    it 'returns self if block given' do
-      expect(my_array.my_each { |x| x + 1 }).to eql(my_array)
+    it "returns self if block is given" do
+      expect(my_array.my_each { |x| x + 3 }).to eql(my_array)
+    end
+  end
+
+  # my_each_with_index
+  describe '#my_each_with_index' do
+    it "returns an enumerator if block not is given" do
+      expect(my_array.my_each_with_index).to be_an Enumerator
+    end
+
+    it "returns self if block is given" do
+      expect(my_array.my_each_with_index { |item, i| "item in array #{item}, index #{i}" }).to eql(my_array)
     end
   end
 
